75163c58a6d455b3fba88909f97cddb988093cfa
smoockpp <smoockpp@gmail.com>
2017-03-29 20:55:02 +0100

    port changed to be taken from var

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ app.get('/logout', function(req, res) {
  **************************/
 
 const port = process.env.PORT || 5000; // Own port or pull from .env file
-app.listen(3000);
+app.listen(port);
 console.log('Listening on ' + port + '!');
 
 
